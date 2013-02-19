b0fe97fb6fbc97b86d63d6c242ad034621635192
Linh Tran <linhtm@icts.vn>
2013-02-19 17:54:18 +0700

    change marker icon

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -54,14 +54,14 @@
         var pinkIcon = L.icon({
           iconUrl: 'http://49.212.161.19:3050/marker_pink.png',
           shadowUrl: 'http://49.212.161.19:3050/marker_shadow.png',
-          popupAnchor: [3, 10],
+          popupAnchor: [10, 10],
         });
         marker = L.marker([parseFloat(lat), parseFloat(lng)], { icon: pinkIcon });
       } else {
         var grayIcon = L.icon({
           iconUrl: 'http://49.212.161.19:3050/marker_gray.png',
           shadowUrl: 'http://49.212.161.19:3050/marker_shadow.png',
-          popupAnchor: [-3, -76],
+          popupAnchor: [10, 10],
         });
         marker = L.marker([parseFloat(lat), parseFloat(lng)], { icon: grayIcon });
       }
