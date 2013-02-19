c08448f323e6cd6a6ab0246f43b46b04df3a25ee
Linh Tran <linhtm@icts.vn>
2013-02-19 11:19:28 +0700

    change username validation

--- routes/portal/parttimer.js
+++ routes/portal/parttimer.js
@@ -209,11 +209,11 @@ Parttimer.prototype.checkEmail = function(req, res) {
  */
 Parttimer.prototype.validateParttimer = function(req, res, next) {
     // Create rule for validate user instance
-    req.check('username', 'ユーザー名は正しくありません').notEmpty().is(/^[a-zA-Z0-9_]+$/);
-    req.check('username', 'ユーザー名は1文字以上、20文字以内でなければなりません').notEmpty().is(/^[a-zA-Z0-9_]+$/).len(1, 20);
+    req.check('username', 'ユーザー名は正しくありません').notEmpty().is(/^\S+$/);
+    req.check('username', 'ユーザー名は1文字以上、20文字以内でなければなりません').notEmpty().is(/^[a-zA-Z0-9_]+$/).len(1, 200);
     req.check('email', 'メールアドレスは正しくありません').notEmpty().isEmail();
     req.check('password', 'パスワードは必須です').notEmpty();
-    req.check('password', 'パスワードは4文字以上、20文字以内でなければなりません').len(4, 20);
+    req.check('password', 'パスワードは4文字以上、20文字以内でなければなりません').len(4, 30);
     req.check('passwordConfirm', 'パスワードとパスワードの確認は一致しなけらばなりません').notEmpty().equals(req.body.password);
 
   // Create the mapped errors array
