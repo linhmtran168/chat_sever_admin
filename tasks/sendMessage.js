06c9b0b410bb29c55964ad87829d55542b6ba09e
Linh Tran <linhtm@icts.vn>
2013-02-22 15:02:30 +0700

    add send message

---
+++ tasks/sendMessage.js
@@ -0,0 +1,10 @@
+var User = require('../../models/user');
+
+process.on('message', function(message) {
+  console.log('Child process - message received: ' + message);
+  process.exit();
+});
+
+process.on('exit', function() {
+  process.send('Finished sending message');
+});
