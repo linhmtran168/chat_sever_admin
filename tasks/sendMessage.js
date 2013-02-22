a7a4a14d05cf2a9c034cf7adb9ac5f8fe7b62071
Linh Tran <linhtm@icts.vn>
2013-02-22 17:38:21 +0700

    send messages to client through redis

--- tasks/sendMessage.js
+++ tasks/sendMessage.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose')
+  , redis = require('redis')
+  , _ = require('lodash')
   , User = require('../models/user');
 
 process.on('message', function(data) {
@@ -24,8 +26,9 @@ process.on('message', function(data) {
       process.exit();
     }
 
-    console.log(admin);
+    //console.log(admin);
 
+    // -- If there is admin user
     // Find all the users according to type of receiver
     var condition, receiverType = parseInt(data.receiver, 10);
     if (receiverType === 1) {
@@ -36,6 +39,7 @@ process.on('message', function(data) {
       condition = { gender: 'female', type: { $ne: 'admin' } };
     }
 
+    // Get all the users with right condition
     User.find(condition, function(err, users) {
       if (err) {
         console.error(err);
@@ -48,9 +52,45 @@ process.on('message', function(data) {
         process.exit();
       }
 
-      console.log(users);
-      process.exit();
+      // Start sending all messages through redis
+      var redisClient = redis.createClient()
+        , multi = redisClient.multi()
+        , currentTimestamp = Math.round(+new Date()/1000);
+      var messageObj = {
+        message: data.message,
+        deviceTimestamp: currentTimestamp,
+        timestamp: currentTimestamp,
+        senderId: admin.id
+      };
+      var messageJSON = JSON.stringify(messageObj);
+
+      // Add commands to redis for each user to node redis queue
+      _.each(users, function(user) {
+        // Create keys in Redis for this user
+        multi.sadd('chat:' + user.id + ':keys', 'chat:' + user.id + ':' + admin.id + ':messages', 'chat:' + user.id + ':' + admin.id + ':lastMessage', 'chat:' + user.id +  ':conversations', redis.print);
+
+        // Add/ update the conversations of user
+        multi.zadd('chat:' + user.id + ':conversations', currentTimestamp, admin.id, redis.print);
+        // Set the last message
+        multi.set('chat:' + user.id + ':' + admin.id + ':lastMessage', messageJSON, redis.print);
+        // Save the messge obj to the list of messages for user
+        multi.rpush('chat:' + user.id + ':' + admin.id + ':messages', messageJSON, redis.print);
+      });
+
+      // Process all the commmand
+      multi.exec(function(err, replies) {
+        if (err) {
+          console.error(err);
+          process.send('Error sending message');
+          process.exit();
+        }
+
+        process.send('Success fully sent the messages - Total commands: ' + replies.length);
+        redisClient.quit();
+        process.exit();
+      });
     });
+
   });
 });
 
