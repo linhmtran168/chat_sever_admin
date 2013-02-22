8a30454db945135c7a0e2a20d6fc28b832ed6828
Linh Tran <linhtm@icts.vn>
2013-02-22 16:21:21 +0700

    add mongoose to send message script

--- tasks/sendMessage.js
+++ tasks/sendMessage.js
@@ -1,10 +1,34 @@
-var User = require('../models/user');
+var mongoose = require('mongoose')
+  , User = require('../models/user');
 
 process.on('message', function(data) {
   console.log('Child process - message received: ' + data.message + '|' + data.receiver);
-  process.exit();
+
+  // Connect to mongodb database
+  if (process.env.NODE_ENV === 'production') {
+    mongoose.connect('mongodb://localhost:27017/ogorin', { user: 'ogorinPro', pass: 'ProOgorinMongo' });
+  } else {
+    mongoose.connect('mongodb://localhost:27017/ogorin', { user: 'ogorin', pass: 'dragonLinh123' });
+  }
+
+  // Get the admin user from the user collection
+  User.findOne({ type: 'admin' }, function(err, admin) {
+    if (err) {
+      console.error(err);
+      process.send('Error getting admin user');
+      process.exit();
+    }
+
+    if (!admin) {
+      process.send('There is no admin user');
+      process.exit();
+    }
+
+    console.log(admin);
+    process.exit();
+  });
 });
 
 process.on('exit', function() {
-  process.send('Finished sending message');
+  process.send('Message sending operation completed');
 });
